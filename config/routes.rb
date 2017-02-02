039802ca345982a2528e8b040a9e64265837b42d
Vedaste Cyizere <revedaste@gmail.com>
2017-02-02 15:48:44 +0200

    created login and logout options

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   post "/users",  to: "users#create"
 
-  
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+
 
 
 end 
